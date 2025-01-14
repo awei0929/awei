f99b91e3e818fd2206c08de04b002ea9a70f2a1b
awei <awei0929@sjzn.com>
2025-01-14 17:05:36 +0800

    Updates

---
+++ assets/index-5QKSA1U9.js
@@ -0,0 +1,17 @@
+import { j as jsxRuntimeExports } from "./index-CTzgvZxn.js";
+function WelCome() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", {
+    style: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "64px"
+    },
+    children: "你好,你这该死的臭逼！！！"
+  });
+}
+export {
+  WelCome as default
+};
---
+++ assets/index-Cgz3k6K0.js
@@ -0,0 +1,14 @@
+import { o as observer, u as useStores, r as reactExports, j as jsxRuntimeExports } from "./index-CTzgvZxn.js";
+function OnePage() {
+  const store = useStores();
+  reactExports.useEffect(() => {
+    store.app.initAppointAll();
+  }, []);
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", {
+    children: "OnePage"
+  });
+}
+const ObservedOnePage = observer(OnePage);
+export {
+  ObservedOnePage as default
+};
