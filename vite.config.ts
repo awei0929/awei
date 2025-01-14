e08434a1e7d9a59b2c6a13cdb91212dc056d6512
awei <awei0929@sjzn.com>
2025-01-14 16:44:18 +0800

    feat(初始化): 配置部署工具

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
   ];
 
   return {
+    base: '/awei',
     resolve: {
       alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
     },
